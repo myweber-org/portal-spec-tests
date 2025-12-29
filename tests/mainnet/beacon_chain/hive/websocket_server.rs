6e899c785f7ab5a06db13e9f15d2e31a0aa92a80
InkspillVerse <inkspill.verse@outlook.com>
2025-12-29 22:08:56 +0000

    Implement a basic WebSocket server using tokio-tungstenite

--- tests/mainnet/beacon_chain/hive/websocket_server.rs
+++ tests/mainnet/beacon_chain/hive/websocket_server.rs
@@ -1,41 +1,48 @@
 use tokio::net::{TcpListener, TcpStream};
 use tokio_tungstenite::tungstenite::protocol::Message;
 use futures_util::{SinkExt, StreamExt};
-use std::error::Error;
+use std::net::SocketAddr;
 
-pub async fn handle_connection(stream: TcpStream) -> Result<(), Box<dyn Error + Send + Sync>> {
-    let ws_stream = tokio_tungstenite::accept_async(stream).await?;
+pub async fn run_websocket_server(addr: SocketAddr) -> Result<(), Box<dyn std::error::Error>> {
+    let listener = TcpListener::bind(&addr).await?;
+    println!("WebSocket server listening on: {}", addr);
+
+    while let Ok((stream, _)) = listener.accept().await {
+        tokio::spawn(handle_connection(stream));
+    }
+    Ok(())
+}
+
+async fn handle_connection(raw_stream: TcpStream) {
+    let addr = raw_stream.peer_addr().unwrap();
+    println!("New connection from: {}", addr);
+
+    let ws_stream = tokio_tungstenite::accept_async(raw_stream)
+        .await
+        .expect("Error during WebSocket handshake");
+
     let (mut write, mut read) = ws_stream.split();
 
     while let Some(msg) = read.next().await {
-        let msg = msg?;
         match msg {
-            Message::Text(text) => {
-                println!("Received text message: {}", text);
-                write.send(Message::Text(text)).await?;
+            Ok(Message::Text(text)) => {
+                println!("Received text from {}: {}", addr, text);
+                let response = format!("Echo: {}", text);
+                if let Err(e) = write.send(Message::Text(response)).await {
+                    eprintln!("Error sending message to {}: {}", addr, e);
+                    break;
+                }
             }
-            Message::Close(_) => {
-                println!("Client disconnected");
+            Ok(Message::Close(_)) => {
+                println!("Client {} disconnected", addr);
+                break;
+            }
+            Err(e) => {
+                eprintln!("Error processing message from {}: {}", addr, e);
                 break;
             }
             _ => {}
         }
     }
-    Ok(())
-}
-
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
-    let addr = "127.0.0.1:8080";
-    let listener = TcpListener::bind(addr).await?;
-    println!("WebSocket server listening on {}", addr);
-
-    while let Ok((stream, _)) = listener.accept().await {
-        tokio::spawn(async move {
-            if let Err(e) = handle_connection(stream).await {
-                eprintln!("Error handling connection: {}", e);
-            }
-        });
-    }
-    Ok(())
+    println!("Connection closed: {}", addr);
 }

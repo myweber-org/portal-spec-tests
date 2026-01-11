ba67b41f56509b69d667e8c7badbffaf4c7aec00
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-11 14:26:21 +0000

    Add a simple WebSocket echo server using tokio-tungstenite

--- tests/mainnet/beacon_chain/hive/websocket_server.rs
+++ tests/mainnet/beacon_chain/hive/websocket_server.rs
@@ -38,4 +38,33 @@ async fn handle_connection(stream: tokio::net::TcpStream) -> Result<(), Box<dyn 
     }
 
     Ok(())
+}use futures_util::{SinkExt, StreamExt};
+use tokio::net::TcpListener;
+use tokio_tungstenite::accept_async;
+use tokio_tungstenite::tungstenite::Message;
+
+#[tokio::main]
+async fn main() {
+    let addr = "127.0.0.1:8080";
+    let listener = TcpListener::bind(addr).await.unwrap();
+    println!("WebSocket server listening on: {}", addr);
+
+    while let Ok((stream, _)) = listener.accept().await {
+        tokio::spawn(handle_connection(stream));
+    }
+}
+
+async fn handle_connection(stream: tokio::net::TcpStream) {
+    let ws_stream = accept_async(stream).await.unwrap();
+    let (mut sender, mut receiver) = ws_stream.split();
+
+    while let Some(Ok(msg)) = receiver.next().await {
+        if let Message::Text(text) = msg {
+            println!("Received: {}", text);
+            let echo_msg = Message::Text(format!("Echo: {}", text));
+            if sender.send(echo_msg).await.is_err() {
+                break;
+            }
+        }
+    }
 }

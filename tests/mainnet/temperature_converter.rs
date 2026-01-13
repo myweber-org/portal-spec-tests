112da42de0eeb6a0290161709d5a0d2c8beca0a1
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-13 16:21:40 +0000

    Add a simple Celsius to Fahrenheit temperature converter function

--- tests/mainnet/temperature_converter.rs
+++ tests/mainnet/temperature_converter.rs
@@ -1,16 +1,10 @@
+
 fn celsius_to_fahrenheit(celsius: f64) -> f64 {
     (celsius * 9.0 / 5.0) + 32.0
-}
-
-fn fahrenheit_to_celsius(fahrenheit: f64) -> f64 {
-    (fahrenheit - 32.0) * 5.0 / 9.0
 }
 
 fn main() {
     let celsius_temp = 25.0;
     let fahrenheit_temp = celsius_to_fahrenheit(celsius_temp);
     println!("{:.1}°C is equal to {:.1}°F", celsius_temp, fahrenheit_temp);
-
-    let converted_back = fahrenheit_to_celsius(fahrenheit_temp);
-    println!("{:.1}°F is equal to {:.1}°C", fahrenheit_temp, converted_back);
 }

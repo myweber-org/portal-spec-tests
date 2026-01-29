8fb5204ab0f8454c132f42b459b50bd00c6b56c1
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-29 01:08:39 +0000

    Add data cleaning utility to filter out invalid entries from a dataset

--- tests/mainnet/beacon_chain/hive/data_cleaner.rs
+++ tests/mainnet/beacon_chain/hive/data_cleaner.rs
@@ -1,39 +1,49 @@
-use std::collections::HashSet;
 
-pub struct DataCleaner<T> {
-    data: Vec<T>,
+use std::collections::HashMap;
+
+pub struct DataCleaner {
+    filters: Vec<Box<dyn Fn(&HashMap<String, String>) -> bool>>,
 }
 
-impl<T> DataCleaner<T>
-where
-    T: Eq + std::hash::Hash + Clone,
-{
-    pub fn new(data: Vec<T>) -> Self {
-        DataCleaner { data }
-    }
-
-    pub fn remove_duplicates(&mut self) -> Vec<T> {
-        let mut seen = HashSet::new();
-        let mut result = Vec::new();
-
-        for item in self.data.iter() {
-            if seen.insert(item) {
-                result.push(item.clone());
-            }
+impl DataCleaner {
+    pub fn new() -> Self {
+        DataCleaner {
+            filters: Vec::new(),
         }
-
-        self.data = result.clone();
-        result
     }
 
-    pub fn get_unique_count(&self) -> usize {
-        let set: HashSet<_> = self.data.iter().collect();
-        set.len()
+    pub fn add_filter<F>(&mut self, filter: F)
+    where
+        F: Fn(&HashMap<String, String>) -> bool + 'static,
+    {
+        self.filters.push(Box::new(filter));
     }
 
-    pub fn is_empty(&self) -> bool {
-        self.data.is_empty()
+    pub fn clean(&self, data: Vec<HashMap<String, String>>) -> Vec<HashMap<String, String>> {
+        data.into_iter()
+            .filter(|entry| self.filters.iter().all(|f| f(entry)))
+            .collect()
     }
+}
+
+pub fn create_default_cleaner() -> DataCleaner {
+    let mut cleaner = DataCleaner::new();
+
+    cleaner.add_filter(|entry| {
+        entry.contains_key("id") && !entry.get("id").unwrap().is_empty()
+    });
+
+    cleaner.add_filter(|entry| {
+        entry.get("timestamp")
+            .and_then(|ts| ts.parse::<u64>().ok())
+            .map_or(false, |timestamp| timestamp > 0)
+    });
+
+    cleaner.add_filter(|entry| {
+        !entry.values().any(|value| value.trim().is_empty())
+    });
+
+    cleaner
 }
 
 #[cfg(test)]
@@ -41,17 +51,34 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_remove_duplicates() {
-        let mut cleaner = DataCleaner::new(vec![1, 2, 2, 3, 4, 4, 5]);
-        let unique = cleaner.remove_duplicates();
-        assert_eq!(unique, vec![1, 2, 3, 4, 5]);
-        assert_eq!(cleaner.get_unique_count(), 5);
+    fn test_cleaner_filters_invalid_data() {
+        let cleaner = create_default_cleaner();
+
+        let mut valid_entry = HashMap::new();
+        valid_entry.insert("id".to_string(), "123".to_string());
+        valid_entry.insert("timestamp".to_string(), "1672531200".to_string());
+        valid_entry.insert("name".to_string(), "test".to_string());
+
+        let mut invalid_entry = HashMap::new();
+        invalid_entry.insert("id".to_string(), "".to_string());
+        invalid_entry.insert("timestamp".to_string(), "0".to_string());
+
+        let data = vec![valid_entry.clone(), invalid_entry];
+        let cleaned = cleaner.clean(data);
+
+        assert_eq!(cleaned.len(), 1);
+        assert_eq!(cleaned[0]["id"], "123");
     }
 
     #[test]
-    fn test_empty_data() {
-        let cleaner: DataCleaner<i32> = DataCleaner::new(vec![]);
-        assert!(cleaner.is_empty());
-        assert_eq!(cleaner.get_unique_count(), 0);
+    fn test_empty_filter_chain() {
+        let cleaner = DataCleaner::new();
+        let mut entry = HashMap::new();
+        entry.insert("test".to_string(), "value".to_string());
+
+        let data = vec![entry];
+        let cleaned = cleaner.clean(data);
+
+        assert_eq!(cleaned.len(), 1);
     }
 }

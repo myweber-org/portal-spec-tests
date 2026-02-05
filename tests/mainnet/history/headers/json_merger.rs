5d187d29778fe14416d854731ab262fe3e66e0aa
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-05 23:40:17 +0000

    Add utility to merge multiple JSON files into a single JSON array

--- tests/mainnet/history/headers/json_merger.rs
+++ tests/mainnet/history/headers/json_merger.rs
@@ -342,4 +342,62 @@ mod tests {
         assert_eq!(result["c"], true);
         assert_eq!(result["d"], serde_json::json!([1, 2, 3]));
     }
+}use serde_json::{json, Value};
+use std::collections::HashSet;
+use std::fs::File;
+use std::io::{BufReader, Read};
+use std::path::Path;
+
+pub fn merge_json_files<P: AsRef<Path>>(paths: &[P], deduplicate: bool) -> Result<Value, Box<dyn std::error::Error>> {
+    let mut merged_array = Vec::new();
+    let mut seen_keys = HashSet::new();
+
+    for path in paths {
+        let file = File::open(path)?;
+        let mut reader = BufReader::new(file);
+        let mut contents = String::new();
+        reader.read_to_string(&mut contents)?;
+
+        let json_value: Value = serde_json::from_str(&contents)?;
+
+        match json_value {
+            Value::Array(arr) => {
+                for item in arr {
+                    if deduplicate {
+                        if let Some(id) = item.get("id").and_then(|v| v.as_str()) {
+                            if seen_keys.insert(id.to_string()) {
+                                merged_array.push(item);
+                            }
+                        } else {
+                            merged_array.push(item);
+                        }
+                    } else {
+                        merged_array.push(item);
+                    }
+                }
+            }
+            Value::Object(obj) => {
+                if deduplicate {
+                    if let Some(id) = obj.get("id").and_then(|v| v.as_str()) {
+                        if seen_keys.insert(id.to_string()) {
+                            merged_array.push(Value::Object(obj));
+                        }
+                    } else {
+                        merged_array.push(Value::Object(obj));
+                    }
+                } else {
+                    merged_array.push(Value::Object(obj));
+                }
+            }
+            _ => return Err("Input JSON must be an array or object".into()),
+        }
+    }
+
+    Ok(Value::Array(merged_array))
+}
+
+pub fn write_merged_json<P: AsRef<Path>>(output_path: P, value: &Value) -> Result<(), Box<dyn std::error::Error>> {
+    let file = File::create(output_path)?;
+    serde_json::to_writer_pretty(file, value)?;
+    Ok(())
 }

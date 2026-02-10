6a47d216b7afcfd825cc5b8462eddacbb4c7b940
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-10 01:29:30 +0000

    Add file encryption utility with AES-256-GCM

--- tests/mainnet/history/bodies/file_encryption.rs
+++ tests/mainnet/history/bodies/file_encryption.rs
@@ -233,4 +233,28 @@ mod tests {
         let decrypted_content = fs::read(decrypted_file.path()).unwrap();
         assert_eq!(decrypted_content, test_data);
     }
+}use aes_gcm::{
+    aead::{Aead, KeyInit, OsRng},
+    Aes256Gcm, Key, Nonce,
+};
+use std::error::Error;
+
+pub fn encrypt_data(plaintext: &[u8], key: &[u8]) -> Result<Vec<u8>, Box<dyn Error>> {
+    let cipher = Aes256Gcm::new(Key::<Aes256Gcm>::from_slice(key));
+    let nonce = Nonce::from_slice(&[0u8; 12]);
+    let ciphertext = cipher.encrypt(nonce, plaintext)?;
+    Ok(ciphertext)
+}
+
+pub fn decrypt_data(ciphertext: &[u8], key: &[u8]) -> Result<Vec<u8>, Box<dyn Error>> {
+    let cipher = Aes256Gcm::new(Key::<Aes256Gcm>::from_slice(key));
+    let nonce = Nonce::from_slice(&[0u8; 12]);
+    let plaintext = cipher.decrypt(nonce, ciphertext)?;
+    Ok(plaintext)
+}
+
+pub fn generate_key() -> [u8; 32] {
+    let mut key = [0u8; 32];
+    OsRng.fill_bytes(&mut key);
+    key
 }

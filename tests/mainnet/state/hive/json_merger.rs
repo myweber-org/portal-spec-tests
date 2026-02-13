6c76d874cc478fda11e735ebaed6b5377d7b9458
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-13 19:54:34 +0000

    Add utility to merge multiple JSON files into a single object

--- tests/mainnet/state/hive/json_merger.rs
+++ tests/mainnet/state/hive/json_merger.rs
@@ -74,3 +74,95 @@ pub fn merge_json_files(file_paths: &[&str]) -> Result<serde_json::Value, Box<dy
 
     Ok(serde_json::Value::Object(merged_map.into_iter().collect()))
 }
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{BufReader, Read};
+use std::path::Path;
+
+type JsonValue = serde_json::Value;
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+pub struct JsonMerger {
+    data: HashMap<String, JsonValue>,
+}
+
+impl JsonMerger {
+    pub fn new() -> Self {
+        JsonMerger {
+            data: HashMap::new(),
+        }
+    }
+
+    pub fn add_file<P: AsRef<Path>>(&mut self, path: P) -> Result<()> {
+        let file = File::open(path.as_ref())?;
+        let mut reader = BufReader::new(file);
+        let mut contents = String::new();
+        reader.read_to_string(&mut contents)?;
+
+        let json_data: JsonValue = serde_json::from_str(&contents)?;
+        
+        if let JsonValue::Object(map) = json_data {
+            for (key, value) in map {
+                self.data.insert(key, value);
+            }
+        } else {
+            return Err("Root JSON element must be an object".into());
+        }
+
+        Ok(())
+    }
+
+    pub fn merge(&self) -> JsonValue {
+        let mut result_map = serde_json::Map::new();
+        for (key, value) in &self.data {
+            result_map.insert(key.clone(), value.clone());
+        }
+        JsonValue::Object(result_map)
+    }
+
+    pub fn merge_files<P: AsRef<Path>>(paths: &[P]) -> Result<JsonValue> {
+        let mut merger = JsonMerger::new();
+        for path in paths {
+            merger.add_file(path)?;
+        }
+        Ok(merger.merge())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io::Write;
+    use tempfile::NamedTempFile;
+
+    fn create_temp_json(content: &str) -> NamedTempFile {
+        let mut file = NamedTempFile::new().unwrap();
+        write!(file, "{}", content).unwrap();
+        file
+    }
+
+    #[test]
+    fn test_basic_merge() {
+        let file1 = create_temp_json(r#"{"a": 1, "b": 2}"#);
+        let file2 = create_temp_json(r#"{"c": 3, "d": 4}"#);
+        
+        let result = JsonMerger::merge_files(&[file1.path(), file2.path()]).unwrap();
+        let obj = result.as_object().unwrap();
+        
+        assert_eq!(obj.get("a").unwrap().as_i64().unwrap(), 1);
+        assert_eq!(obj.get("b").unwrap().as_i64().unwrap(), 2);
+        assert_eq!(obj.get("c").unwrap().as_i64().unwrap(), 3);
+        assert_eq!(obj.get("d").unwrap().as_i64().unwrap(), 4);
+    }
+
+    #[test]
+    fn test_overwrite_behavior() {
+        let file1 = create_temp_json(r#"{"key": "first"}"#);
+        let file2 = create_temp_json(r#"{"key": "second"}"#);
+        
+        let result = JsonMerger::merge_files(&[file1.path(), file2.path()]).unwrap();
+        let obj = result.as_object().unwrap();
+        
+        assert_eq!(obj.get("key").unwrap().as_str().unwrap(), "second");
+    }
+}

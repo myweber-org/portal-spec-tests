a02ebcb783c95e679f3cb75a666e2f3262e31a88
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-13 16:22:44 +0000

    Add a simple WebSocket echo server using tokio-tungstenite

--- tests/mainnet/beacon_chain/websocket_server.rs
+++ tests/mainnet/beacon_chain/websocket_server.rs
@@ -43,4 +43,36 @@ async fn main() {
     while let Ok((stream, _)) = listener.accept().await {
         tokio::spawn(handle_connection(stream));
     }
+}use futures_util::{SinkExt, StreamExt};
+use tokio::net::{TcpListener, TcpStream};
+use tokio_tungstenite::tungstenite::Message;
+use tokio_tungstenite::WebSocketStream;
+
+async fn handle_connection(stream: TcpStream) {
+    let ws_stream = tokio_tungstenite::accept_async(stream).await.unwrap();
+    let (mut write, mut read) = ws_stream.split();
+
+    while let Some(msg) = read.next().await {
+        match msg {
+            Ok(Message::Text(text)) => {
+                let echo_msg = Message::Text(format!("Echo: {}", text));
+                if write.send(echo_msg).await.is_err() {
+                    break;
+                }
+            }
+            Ok(Message::Close(_)) => break,
+            Err(_) => break,
+            _ => {}
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
+    println!("WebSocket echo server listening on ws://127.0.0.1:8080");
+
+    while let Ok((stream, _)) = listener.accept().await {
+        tokio::spawn(handle_connection(stream));
+    }
 }

86a7d7f3255c8484d88e131da6bb1661baa553b4
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-02-15 16:49:27 +0000

    Add utility function to filter out non-numeric strings from a vector

--- tests/mainnet/beacon_chain/historical_summaries_with_proof/deneb/data_cleaner.rs
+++ tests/mainnet/beacon_chain/historical_summaries_with_proof/deneb/data_cleaner.rs
@@ -468,4 +468,30 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
 
     Ok(())
+}use std::str::FromStr;
+
+pub fn filter_numeric_strings<T: FromStr>(items: Vec<String>) -> Vec<T> {
+    items
+        .into_iter()
+        .filter_map(|s| s.parse::<T>().ok())
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_filter_numeric_strings() {
+        let input = vec![
+            "42".to_string(),
+            "hello".to_string(),
+            "3.14".to_string(),
+            "world".to_string(),
+            "-7".to_string(),
+        ];
+        
+        let result: Vec<i32> = filter_numeric_strings(input);
+        assert_eq!(result, vec![42, -7]);
+    }
 }

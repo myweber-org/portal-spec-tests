efaca500b9613d8ca26995bff74df736a48dc200
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-02-17 19:40:17 +0000

    Add file encryption utility with AES-256-GCM

--- tests/mainnet/history/bodies/file_encryption.rs
+++ tests/mainnet/history/bodies/file_encryption.rs
@@ -182,4 +182,53 @@ mod tests {
         
         Ok(())
     }
+}use aes_gcm::{
+    aead::{Aead, KeyInit, OsRng},
+    Aes256Gcm, Key, Nonce,
+};
+use std::error::Error;
+
+pub fn encrypt_file(data: &[u8], key: &[u8; 32]) -> Result<Vec<u8>, Box<dyn Error>> {
+    let cipher = Aes256Gcm::new(Key::<Aes256Gcm>::from_slice(key));
+    let nonce = Nonce::generate(&mut OsRng);
+    
+    let mut ciphertext = cipher.encrypt(&nonce, data)?;
+    let mut result = Vec::with_capacity(nonce.len() + ciphertext.len());
+    result.extend_from_slice(nonce.as_slice());
+    result.append(&mut ciphertext);
+    
+    Ok(result)
+}
+
+pub fn decrypt_file(encrypted_data: &[u8], key: &[u8; 32]) -> Result<Vec<u8>, Box<dyn Error>> {
+    if encrypted_data.len() < 12 {
+        return Err("Invalid encrypted data".into());
+    }
+    
+    let (nonce_bytes, ciphertext) = encrypted_data.split_at(12);
+    let cipher = Aes256Gcm::new(Key::<Aes256Gcm>::from_slice(key));
+    let nonce = Nonce::from_slice(nonce_bytes);
+    
+    let plaintext = cipher.decrypt(nonce, ciphertext)?;
+    Ok(plaintext)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rand::RngCore;
+
+    #[test]
+    fn test_encryption_roundtrip() {
+        let mut key = [0u8; 32];
+        rand::thread_rng().fill_bytes(&mut key);
+        
+        let original_data = b"Secret file content that needs protection";
+        
+        let encrypted = encrypt_file(original_data, &key).unwrap();
+        let decrypted = decrypt_file(&encrypted, &key).unwrap();
+        
+        assert_eq!(original_data.to_vec(), decrypted);
+        assert_ne!(original_data, encrypted.as_slice());
+    }
 }

9c1b738aba513d444cb77209b6a7bd76b6e83768
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-02-19 19:43:07 +0000

    Implement a basic WebSocket echo server using tokio-tungstenite

--- tests/websocket_server.rs
+++ tests/websocket_server.rs
@@ -1,33 +1,45 @@
+
+use futures_util::{SinkExt, StreamExt};
 use tokio::net::TcpListener;
 use tokio_tungstenite::accept_async;
-use futures_util::{SinkExt, StreamExt};
-use std::error::Error;
+use tokio_tungstenite::tungstenite::Message;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "127.0.0.1:8080";
     let listener = TcpListener::bind(addr).await?;
-    println!("WebSocket echo server listening on: {}", addr);
+    println!("WebSocket server listening on: {}", addr);
 
     while let Ok((stream, _)) = listener.accept().await {
-        tokio::spawn(async move {
-            if let Err(e) = handle_connection(stream).await {
-                eprintln!("Error handling connection: {}", e);
-            }
-        });
+        tokio::spawn(handle_connection(stream));
     }
+
     Ok(())
 }
 
-async fn handle_connection(stream: tokio::net::TcpStream) -> Result<(), Box<dyn Error>> {
+async fn handle_connection(stream: tokio::net::TcpStream) -> Result<(), Box<dyn std::error::Error>> {
     let ws_stream = accept_async(stream).await?;
-    let (mut write, mut read) = ws_stream.split();
+    let (mut sender, mut receiver) = ws_stream.split();
 
-    while let Some(msg) = read.next().await {
-        let msg = msg?;
-        if msg.is_text() || msg.is_binary() {
-            write.send(msg).await?;
+    while let Some(msg) = receiver.next().await {
+        match msg {
+            Ok(Message::Text(text)) => {
+                println!("Received text message: {}", text);
+                sender.send(Message::Text(format!("Echo: {}", text))).await?;
+            }
+            Ok(Message::Close(_)) => {
+                println!("Client disconnected");
+                break;
+            }
+            Ok(_) => {
+                println!("Received non-text message, ignoring");
+            }
+            Err(e) => {
+                eprintln!("Error receiving message: {}", e);
+                break;
+            }
         }
     }
+
     Ok(())
 }

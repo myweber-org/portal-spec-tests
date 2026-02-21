005289344028735c6f0fa28f2e2792b6d1f40348
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-21 18:28:57 +0000

    Add a simple WebSocket echo server using tokio-tungstenite

--- tests/mainnet/history/bodies/websocket_server.rs
+++ tests/mainnet/history/bodies/websocket_server.rs
@@ -40,4 +40,42 @@ async fn handle_connection(raw_stream: tokio::net::TcpStream) {
             }
         }
     }
+}use futures_util::{SinkExt, StreamExt};
+use tokio::net::TcpListener;
+use tokio_tungstenite::accept_async;
+use tokio_tungstenite::tungstenite::Message;
+
+#[tokio::main]
+async fn main() {
+    let addr = "127.0.0.1:8080";
+    let listener = TcpListener::bind(addr).await.unwrap();
+    println!("WebSocket server listening on {}", addr);
+
+    while let Ok((stream, _)) = listener.accept().await {
+        tokio::spawn(handle_connection(stream));
+    }
+}
+
+async fn handle_connection(stream: tokio::net::TcpStream) {
+    let ws_stream = accept_async(stream).await.unwrap();
+    let (mut ws_sender, mut ws_receiver) = ws_stream.split();
+
+    while let Some(msg) = ws_receiver.next().await {
+        match msg {
+            Ok(Message::Text(text)) => {
+                println!("Received: {}", text);
+                let echo_msg = Message::Text(format!("Echo: {}", text));
+                ws_sender.send(echo_msg).await.unwrap();
+            }
+            Ok(Message::Close(_)) => {
+                println!("Client disconnected");
+                break;
+            }
+            Err(e) => {
+                eprintln!("Error receiving message: {}", e);
+                break;
+            }
+            _ => {}
+        }
+    }
 }

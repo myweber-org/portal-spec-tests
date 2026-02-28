5fd5f5b7df2224333adcad94cfd3c6b7bcc6ebc0
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-28 14:16:38 +0000

    Implement a basic WebSocket echo server using tokio-tungstenite

--- portal-spec-test-utils-rs/websocket_server.rs
+++ portal-spec-test-utils-rs/websocket_server.rs
@@ -1,47 +1,35 @@
-
-use futures_util::{SinkExt, StreamExt};
-use tokio::net::TcpListener;
+use tokio::net::{TcpListener, TcpStream};
 use tokio_tungstenite::tungstenite::protocol::Message;
-use tokio_tungstenite::accept_async;
+use futures_util::{SinkExt, StreamExt};
+use std::error::Error;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "127.0.0.1:8080";
-    let listener = TcpListener::bind(addr).await?;
-    println!("WebSocket server listening on {}", addr);
+async fn handle_connection(stream: TcpStream) -> Result<(), Box<dyn Error + Send + Sync>> {
+    let ws_stream = tokio_tungstenite::accept_async(stream).await?;
+    let (mut ws_sender, mut ws_receiver) = ws_stream.split();
 
-    while let Ok((stream, _)) = listener.accept().await {
-        tokio::spawn(async move {
-            if let Err(e) = handle_connection(stream).await {
-                eprintln!("Error handling connection: {}", e);
-            }
-        });
+    while let Some(msg) = ws_receiver.next().await {
+        let msg = msg?;
+        if msg.is_close() {
+            break;
+        }
+        if msg.is_text() || msg.is_binary() {
+            ws_sender.send(msg).await?;
+        }
     }
     Ok(())
 }
 
-async fn handle_connection(stream: tokio::net::TcpStream) -> Result<(), Box<dyn std::error::Error>> {
-    let ws_stream = accept_async(stream).await?;
-    let (mut write, mut read) = ws_stream.split();
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
+    let listener = TcpListener::bind("127.0.0.1:8080").await?;
+    println!("WebSocket echo server listening on ws://127.0.0.1:8080");
 
-    while let Some(msg) = read.next().await {
-        match msg {
-            Ok(Message::Text(text)) => {
-                println!("Received text message: {}", text);
-                write.send(Message::Text(text)).await?;
+    while let Ok((stream, _)) = listener.accept().await {
+        tokio::spawn(async move {
+            if let Err(e) = handle_connection(stream).await {
+                eprintln!("Connection error: {}", e);
             }
-            Ok(Message::Close(_)) => {
-                println!("Client closed connection");
-                break;
-            }
-            Ok(other) => {
-                println!("Received other message type: {:?}", other);
-            }
-            Err(e) => {
-                eprintln!("Error reading message: {}", e);
-                break;
-            }
-        }
+        });
     }
     Ok(())
 }

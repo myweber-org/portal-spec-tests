923f0736868f7f63d622ca4ca66fa7c7fbdb4182
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-03-07 00:34:24 +0000

    Implement a basic WebSocket server using tokio-tungstenite

--- tests/mainnet/history/accumulator/websocket_server.rs
+++ tests/mainnet/history/accumulator/websocket_server.rs
@@ -1,41 +1,42 @@
-use futures_util::{SinkExt, StreamExt};
+
 use tokio::net::TcpListener;
 use tokio_tungstenite::accept_async;
-use tokio_tungstenite::tungstenite::Message;
+use futures_util::{SinkExt, StreamExt};
+use std::error::Error;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> Result<(), Box<dyn Error>> {
     let addr = "127.0.0.1:8080";
     let listener = TcpListener::bind(addr).await?;
-    println!("WebSocket echo server listening on: {}", addr);
+    println!("WebSocket server listening on {}", addr);
 
     while let Ok((stream, _)) = listener.accept().await {
-        tokio::spawn(async move {
-            match accept_async(stream).await {
-                Ok(ws_stream) => {
-                    let (mut write, mut read) = ws_stream.split();
-                    while let Some(msg) = read.next().await {
-                        match msg {
-                            Ok(Message::Text(text)) => {
-                                if let Err(e) = write.send(Message::Text(text)).await {
-                                    eprintln!("Error sending message: {}", e);
-                                    break;
-                                }
-                            }
-                            Ok(Message::Close(_)) => {
-                                break;
-                            }
-                            Err(e) => {
-                                eprintln!("Error reading message: {}", e);
-                                break;
-                            }
-                            _ => {}
-                        }
-                    }
-                }
-                Err(e) => eprintln!("Error during WebSocket handshake: {}", e),
-            }
-        });
+        tokio::spawn(handle_connection(stream));
     }
+
+    Ok(())
+}
+
+async fn handle_connection(stream: tokio::net::TcpStream) -> Result<(), Box<dyn Error>> {
+    let ws_stream = accept_async(stream).await?;
+    let (mut write, mut read) = ws_stream.split();
+
+    while let Some(message) = read.next().await {
+        match message {
+            Ok(msg) => {
+                if msg.is_text() || msg.is_binary() {
+                    let response = format!("Echo: {}", msg);
+                    write.send(tokio_tungstenite::tungstenite::Message::Text(response)).await?;
+                } else if msg.is_close() {
+                    break;
+                }
+            }
+            Err(e) => {
+                eprintln!("Error reading message: {}", e);
+                break;
+            }
+        }
+    }
+
     Ok(())
 }

e6990d412130bdc02a9f19cbcc6fe645a98c459b
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-03-09 14:23:13 +0000

    Implement a basic WebSocket server using tokio-tungstenite

--- tests/mainnet/beacon_chain/historical_summaries_with_proof/websocket_server.rs
+++ tests/mainnet/beacon_chain/historical_summaries_with_proof/websocket_server.rs
@@ -30,4 +30,50 @@ async fn main() {
     while let Ok((stream, _)) = listener.accept().await {
         tokio::spawn(handle_connection(stream));
     }
+}use tokio::net::TcpListener;
+use tokio_tungstenite::accept_async;
+use futures_util::{SinkExt, StreamExt};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "127.0.0.1:8080";
+    let listener = TcpListener::bind(addr).await?;
+    println!("WebSocket server listening on {}", addr);
+
+    while let Ok((stream, _)) = listener.accept().await {
+        tokio::spawn(handle_connection(stream));
+    }
+    Ok(())
+}
+
+async fn handle_connection(stream: tokio::net::TcpStream) {
+    let ws_stream = match accept_async(stream).await {
+        Ok(ws) => ws,
+        Err(e) => {
+            eprintln!("Error during WebSocket handshake: {}", e);
+            return;
+        }
+    };
+
+    let (mut write, mut read) = ws_stream.split();
+
+    while let Some(message) = read.next().await {
+        match message {
+            Ok(msg) => {
+                if msg.is_text() || msg.is_binary() {
+                    let response = format!("Echo: {}", msg);
+                    if let Err(e) = write.send(response.into()).await {
+                        eprintln!("Error sending message: {}", e);
+                        break;
+                    }
+                } else if msg.is_close() {
+                    break;
+                }
+            }
+            Err(e) => {
+                eprintln!("Error receiving message: {}", e);
+                break;
+            }
+        }
+    }
 }
